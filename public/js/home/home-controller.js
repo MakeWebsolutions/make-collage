b129054b41b7080e1d095c94383ea3891d8c1674
Stian Pedersen <stian.b.pedersen@gmail.com>
2016-01-22 15:40:34 +0100

    update

--- public/js/home/home-controller.js
+++ public/js/home/home-controller.js
@@ -1,6 +1,8 @@
 angular.module('make-collage')
-  .controller('HomeController', ['$scope', '$http', function ($scope, $http) {
+  .controller('HomeController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
   	$scope.loading = false;
+
+  	$scope.userid = $routeParams.userid;
 
   	$scope.connect = function() {
   		$scope.loading = true;
